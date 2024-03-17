cd97be94714b02d40d542b4a2e184469609246f7
EkberAkh <ekber.0303@gmail.com>
2024-03-17 12:22:43 +0400

    add starter code

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  [language, setLanguage] = useState("es");
+  [text, setText] = useState("");
+  return <div></div>;
+}
+
+export default App;
